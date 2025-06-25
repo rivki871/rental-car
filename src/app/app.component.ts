d51edb44c3bf683867da1d73ab4e8ba8d26bb5bf
R <rivki871@gmail.com>
2025-06-25 13:43:49 +0300

    Deploy to GitHub Pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RentalCalculatorComponent } from "./rental-calculator/rental-calculator.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RentalCalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
